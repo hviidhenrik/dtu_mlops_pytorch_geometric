1b64d4993628271a557cb755f5f7fab72c1c7ffa
bhastrup <bjarke.hastrup@gmail.com>
2022-01-20 15:51:15 +0100

    upload molecule_handler.py

---
+++ src/models/molecule_handler.py
@@ -0,0 +1,48 @@
+import io
+from abc import ABC
+
+import torch
+from ts.torch_handler.base_handler import BaseHandler
+
+
+
+class MoleculeHandler(BaseHandler, ABC):
+    """
+    Handler class for molecules
+    """
+    def initialize(self, context):
+        super().initialize(context)
+
+
+    def preprocess(self, data):
+        """The preprocess function converts the input data to a float tensor
+        Args:
+            data (List): Input data from the request is in the form of a Tensor
+        Returns:
+            list : The preprocess function returns the input image as a list of float tensors.
+        """
+        images = []
+
+        for row in data:
+            # Compat layer: normally the envelope should just return the data
+            # directly, but older versions of Torchserve didn't have envelope.
+            image = row.get("data") or row.get("body")
+            if isinstance(image, str):
+                # if the image is a string of bytesarray.
+                image = base64.b64decode(image)
+
+            # If the image is sent as bytesarray
+            if isinstance(image, (bytearray, bytes)):
+                image = Image.open(io.BytesIO(image))
+                image = self.image_processing(image)
+            else:
+                # if the image is a list
+                image = torch.FloatTensor(image)
+
+            images.append(image)
+
+        return torch.stack(images).to(self.device)
+
+    def get_insights(self, tensor_data, _, target=0):
+        print("input shape", tensor_data.shape)
+        return self.ig.attribute(tensor_data, target=target, n_steps=15).tolist()
